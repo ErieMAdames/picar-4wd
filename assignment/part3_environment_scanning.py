4b53a9c363f95651189052961c662dfb7474880e
Erie M. Adames <erieadames@gmail.com>
2024-08-31 22:58:06 -0400

    part 3 environment scanning init

--- assignment/part3_environment_scanning.py
+++ assignment/part3_environment_scanning.py
@@ -4,8 +4,20 @@ speed = 30
 
 def main():
     print('Starting Part 3: Environment Scanning')
+    current_angle = 0
+    us_step = pc4.STEP
+    scan_list = []
     while True:
-        scan_list = pc4.scan_step(35)
+        # scan_list = pc4.scan_step(35)
+        current_angle += us_step
+        if current_angle >= pc4.max_angle:
+            current_angle = pc4.max_angle
+            us_step = -pc4.STEP
+        elif current_angle <= pc4.min_angle:
+            current_angle = pc4.min_angle
+            us_step = pc4.STEP
+        distance = pc4.get_distance_at(current_angle)
+        print(str(current_angle) + ' | ' + str(distance))
         # print(str(pc4.current_angle) + ' | ' + str(pc4.get_distance_at(pc4.current_angle)))
         # if not scan_list:
         #     continue
