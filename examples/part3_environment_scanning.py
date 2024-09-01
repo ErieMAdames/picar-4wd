b755a0b7eee723929398365310d1ecbf77a382da
Erie Adames <eriea@erie.lan>
2024-08-31 22:07:48 -0400

    part 3 environment scanning init

--- examples/part3_environment_scanning.py
+++ examples/part3_environment_scanning.py
@@ -3,11 +3,13 @@ import picar_4wd as fc
 speed = 30
 
 def main():
+    print('Starting Part 3: Environment Scanning')
     while True:
         scan_list = fc.scan_step(35)
         if not scan_list:
             continue
         tmp = scan_list[3:7]
+        print(scan_list)
         print(tmp)
         if tmp != [2,2,2,2]:
             fc.turn_right(speed)
