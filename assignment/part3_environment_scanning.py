82359db5f00a4deed2229e60449e5af911c844dd
Erie M. Adames <erieadames@gmail.com>
2024-08-31 23:47:09 -0400

    part 3 environment scanning init

--- assignment/part3_environment_scanning.py
+++ assignment/part3_environment_scanning.py
@@ -13,7 +13,6 @@ def main():
     left = True
     counter = 0
     while True:
-        # scan_list = pc4.scan_step(35)
         current_angle += us_step
         if current_angle >= pc4.max_angle:
             current_angle = pc4.max_angle
@@ -22,10 +21,9 @@ def main():
             current_angle = pc4.min_angle
             us_step = pc4.STEP
         distance = pc4.get_distance_at(current_angle)
-        # print(str(current_angle) + ' | ' + str(distance))
         distances.append(distance)
         if len(distances) == 10:
-            distances_map = map(lambda x: x < 30 and x != -2, distances)
+            distances_map = map(lambda x: x < 15 and x != -2, distances)
             stop = reduce(lambda x, y: x or y, distances_map)
             if stop:
                 pc4.backward(speed)
