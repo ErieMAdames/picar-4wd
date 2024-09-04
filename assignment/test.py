97668c97a7ba730975ac12b84cf4c711056ad3ec
Erie M. Adames <erieadames@gmail.com>
2024-09-04 18:22:04 -0400

    test

--- assignment/test.py
+++ assignment/test.py
@@ -1,58 +1,74 @@
-import cv2
-import sys
-import utils
+# import cv2
+# import sys
+# import utils
+# import time
+# from picamera2 import Picamera2
+
+# picam2 = Picamera2()
+# picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+# picam2.start()
+
+
+# # Variables to calculate FPS
+# counter, fps = 0, 0
+
+# width = 640
+# height = 480
+# # Start capturing video input from the camera
+# cap = cv2.VideoCapture(0)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+# # Visualization parameters
+# row_size = 20  # pixels
+# left_margin = 24  # pixels
+# text_color = (0, 0, 255)  # red
+# font_size = 1
+# font_thickness = 1
+# fps_avg_frame_count = 10
+
+# # Continuously capture images from the camera and run inference
+# while cap.isOpened():
+#     success, image = cap.read()
+#     if not success:
+#         sys.exit(
+#             'ERROR: Unable to read from webcam. Please verify your webcam settings.'
+#         )
+
+#     counter += 1
+#     image = cv2.flip(image, 1)
+
+#     # Convert the image from BGR to RGB as required by the TFLite model.
+#     rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+#     # Draw keypoints and edges on input image
+#     image = utils.visualize(image,)
+
+#     # Calculate the FPS
+#     if counter % fps_avg_frame_count == 0:
+#         end_time = time.time()
+#         fps = fps_avg_frame_count / (end_time - start_time)
+#         start_time = time.time()
+
+#     # Show the FPS
+#     fps_text = 'FPS = {:.1f}'.format(fps)
+#     text_location = (left_margin, row_size)
+#     cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+#                 font_size, text_color, font_thickness)
+
+#     # Stop the program if the ESC key is pressed.
+#     if cv2.waitKey(1) == 27:
+#         break
+#     cv2.imshow('object_detector', image)
+
+# cap.release()
+# cv2.destroyAllWindows()
+from picamera2 import Picamera2, Preview
 import time
-# Variables to calculate FPS
-counter, fps = 0, 0
-
-width = 640
-height = 480
-# Start capturing video input from the camera
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-
-# Visualization parameters
-row_size = 20  # pixels
-left_margin = 24  # pixels
-text_color = (0, 0, 255)  # red
-font_size = 1
-font_thickness = 1
-fps_avg_frame_count = 10
-
-# Continuously capture images from the camera and run inference
-while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-        sys.exit(
-            'ERROR: Unable to read from webcam. Please verify your webcam settings.'
-        )
-
-    counter += 1
-    image = cv2.flip(image, 1)
-
-    # Convert the image from BGR to RGB as required by the TFLite model.
-    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    # Draw keypoints and edges on input image
-    image = utils.visualize(image,)
-
-    # Calculate the FPS
-    if counter % fps_avg_frame_count == 0:
-        end_time = time.time()
-        fps = fps_avg_frame_count / (end_time - start_time)
-        start_time = time.time()
-
-    # Show the FPS
-    fps_text = 'FPS = {:.1f}'.format(fps)
-    text_location = (left_margin, row_size)
-    cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                font_size, text_color, font_thickness)
-
-    # Stop the program if the ESC key is pressed.
-    if cv2.waitKey(1) == 27:
-        break
-    cv2.imshow('object_detector', image)
-
-cap.release()
-cv2.destroyAllWindows()
+picam2 = Picamera2()
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
+picam2.start_preview(Preview.QTGL)
+picam2.start()
+time.sleep(2)
+picam2.capture_file("test.jpg")
