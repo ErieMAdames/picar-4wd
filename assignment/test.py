984bb303da00de8b226ebb7e99d50d12c11f4386
Erie M. Adames <erieadames@gmail.com>
2024-09-04 22:21:24 -0400

    test

--- assignment/test.py
+++ assignment/test.py
@@ -2,15 +2,22 @@ import cv2
 import time
 from picamera2 import Picamera2
 import numpy as np
+from tflite_support.task import core
 from tflite_support.task import processor
-
+from tflite_support.task import vision
 _MARGIN = 10  # pixels
 _ROW_SIZE = 10  # pixels
 _FONT_SIZE = 1
 _FONT_THICKNESS = 1
 _TEXT_COLOR = (0, 0, 255)  # red
 
-
+ # Visualization parameters
+row_size = 20  # pixels
+left_margin = 24  # pixels
+text_color = (0, 0, 255)  # red
+font_size = 1
+font_thickness = 1
+fps_avg_frame_count = 10
 def visualize(
     image: np.ndarray,
     detection_result: processor.DetectionResult,
@@ -42,7 +49,14 @@ def visualize(
                 _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
 
   return image
-
+# Initialize the object detection model
+base_options = core.BaseOptions(
+    file_name=model, use_coral=enable_edgetpu, num_threads=num_threads)
+detection_options = processor.DetectionOptions(
+    max_results=3, score_threshold=0.3)
+options = vision.ObjectDetectorOptions(
+    base_options=base_options, detection_options=detection_options)
+detector = vision.ObjectDetector.create_from_options(options)
 picam2 = Picamera2()
 picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
 picam2.start()
@@ -75,14 +89,20 @@ while True:
     # Convert the image from BGR to RGB as required by the TFLite model.
     rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+    # Create a TensorImage object from the RGB image.
+    input_tensor = vision.TensorImage.create_from_array(rgb_image)
+
+    # Run object detection estimation using the model.
+    detection_result = detector.detect(input_tensor)
+
     # Draw keypoints and edges on input image
-    image = visualize(image,)
+    image = utils.visualize(image, detection_result)
 
     # Calculate the FPS
     if counter % fps_avg_frame_count == 0:
-        end_time = time.time()
-        fps = fps_avg_frame_count / (end_time - start_time)
-        start_time = time.time()
+      end_time = time.time()
+      fps = fps_avg_frame_count / (end_time - start_time)
+      start_time = time.time()
 
     # Show the FPS
     fps_text = 'FPS = {:.1f}'.format(fps)
@@ -92,7 +112,8 @@ while True:
 
     # Stop the program if the ESC key is pressed.
     if cv2.waitKey(1) == 27:
-        break
+      break
     cv2.imshow('object_detector', image)
+
 
 cv2.destroyAllWindows()
