dd344d96ed15b813325eb4a0dd6a5bc1bce4cc50
Erie M. Adames <erieadames@gmail.com>
2024-09-07 16:38:59 -0400

    setup

--- assignment/part5_driving.py
+++ assignment/part5_driving.py
@@ -188,7 +188,7 @@ def right_encoder_callback_falling(channel):
 
 # Add event detection for rising edges
 GPIO.add_event_detect(LEFT_ENCODER_PIN, GPIO.RISING, callback=left_encoder_callback_rising)
-# GPIO.add_event_detect(RIGHT_ENCODER_PIN, GPIO.RISING, callback=right_encoder_callback_rising)
+GPIO.add_event_detect(RIGHT_ENCODER_PIN, GPIO.RISING, callback=right_encoder_callback_rising)
 # GPIO.add_event_detect(LEFT_ENCODER_PIN, GPIO.FALLING, callback=left_encoder_callback_falling)
 # GPIO.add_event_detect(RIGHT_ENCODER_PIN, GPIO.FALLING, callback=right_encoder_callback_falling)
 # pc4.forward(20)
