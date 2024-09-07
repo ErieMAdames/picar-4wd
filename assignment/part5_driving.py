3c54542d14f1fcb63a0e8cc36a06a6d05ea79067
Erie M. Adames <erieadames@gmail.com>
2024-09-07 18:47:22 -0400

    setup

--- assignment/part5_driving.py
+++ assignment/part5_driving.py
@@ -214,11 +214,11 @@ def turn_continuous(speed=50):
         while True:
             pc4.left_front.set_power(speed)
             pc4.left_rear.set_power(speed)
-            time.sleep(0.5) 
+            time.sleep(0.25) 
             pc4.stop()
             pc4.right_front.set_power(-speed)
             pc4.right_rear.set_power(-speed)
-            time.sleep(0.5) 
+            time.sleep(0.25) 
             pc4.stop() # Adjust the sleep time to prevent excessive CPU usage
     except KeyboardInterrupt:
         # Stop the car when interrupted (Ctrl+C)
