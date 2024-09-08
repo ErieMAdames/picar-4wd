bf1c8957fcc0a781355e21466b51236267011d61
Erie M. Adames <erieadames@gmail.com>
2024-09-07 20:52:16 -0400

    setup

--- assignment/part5_driving.py
+++ assignment/part5_driving.py
@@ -278,7 +278,7 @@ client_thread.start()
 time.sleep(2)
 # Start the calibration process
 start_time = time.time()
-turn_continuous(10)
+turn_continuous(30)
 end_time = time.time()
 
 # Calculate and display the time taken
