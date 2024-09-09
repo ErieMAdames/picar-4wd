6fa5750ad161ff3a26f2ae16aa8230824447a9fb
Erie M. Adames <erieadames@gmail.com>
2024-09-08 21:08:14 -0400

    setup

--- assignment/part5_driving.py
+++ assignment/part5_driving.py
@@ -194,7 +194,7 @@ class AvoidObjects():
 
     def turn(self, right=True, angle=90, speed=30):
         print(right)
-        print(speed)
+        print(angle)
         print('turning')
         start_angle = self.turning_angle
         a = self.turning_angle - start_angle
