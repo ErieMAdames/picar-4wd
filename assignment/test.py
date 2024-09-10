c80aebdc2a2b392439e720267aa03ab44f95411f
Erie M. Adames <erieadames@gmail.com>
2024-09-09 22:05:59 -0400

    setup

--- assignment/test.py
+++ assignment/test.py
@@ -9,7 +9,7 @@ from tflite_support.task import vision
 
 _MARGIN = 10  # pixels
 _ROW_SIZE = 10  # pixels
-_FONT_SIZE = 1
+_FONT_SIZE = 2
 _FONT_THICKNESS = 1
 _TEXT_COLOR = (0, 0, 255)  # red
 
@@ -41,20 +41,21 @@ def visualize(
     print(detection.categories)
     print(category_name)
     # Draw bounding_box
-    bbox = detection.bounding_box
-    start_point = bbox.origin_x, bbox.origin_y
-    end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
-    cv2.rectangle(image, start_point, end_point, _TEXT_COLOR, 3)
+    if (category_name == 'stop sight'):
+      bbox = detection.bounding_box
+      start_point = bbox.origin_x, bbox.origin_y
+      end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
+      cv2.rectangle(image, start_point, end_point, _TEXT_COLOR, 3)
 
-    # Draw label and score
-    category = detection.categories[0]
-    category_name = category.category_name
-    probability = round(category.score, 2)
-    result_text = category_name + ' (' + str(probability) + ')'
-    text_location = (_MARGIN + bbox.origin_x,
-                     _MARGIN + _ROW_SIZE + bbox.origin_y)
-    cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
+      # Draw label and score
+      category = detection.categories[0]
+      category_name = category.category_name
+      probability = round(category.score, 2)
+      result_text = category_name + ' (' + str(probability) + ')'
+      text_location = (_MARGIN + bbox.origin_x,
+                      _MARGIN + _ROW_SIZE + bbox.origin_y)
+      cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                  _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
 
   return image
 # Initialize the object detection model
