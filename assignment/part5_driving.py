1d70ad8ebfc637ef67427d888b470337206b0d3e
Erie M. Adames <erieadames@gmail.com>
2024-09-10 21:41:09 -0400

    test

--- assignment/part5_driving.py
+++ assignment/part5_driving.py
@@ -43,6 +43,7 @@ class AvoidObjects():
         # imu_thread.daemon = True
         # imu_thread.start()
         self.turn_right()
+        self.turn_left()
         exit()
         traveled = self.go_distance(1, True)
         if traveled < 1:
@@ -232,13 +233,21 @@ class AvoidObjects():
             error = abs((a - angle)/angle)
         pc4.stop()
     def turn_left(self, angle=90, speed=30):
-        start_angle = self.turning_angle
-        a = self.turning_angle - start_angle
+        prev_time = time.time()
+        start_angle = 0
+        a = 0
         a = abs((a + 180) % 360 - 180)
         pc4.turn_left(speed)
         while a < angle:
             a = self.turning_angle - start_angle
             a = abs((a + 180) % 360 - 180)
+            current_time = time.time()
+            dt = current_time - prev_time  # Time difference
+            prev_time = current_time
+            gyro_data = self.imu.get_gyro_data()
+            gyro_z = gyro_data['z'] - self.imu_offsets['z']
+            # Integrate angular velocity over time
+            self.turning_angle += gyro_z * dt
             error = abs((a - angle)/angle)
         pc4.stop()
 
