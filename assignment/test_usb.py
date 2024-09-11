7aaf84f5162796ff1fa4c03379e3ebd8bcaf12f8
Erie M. Adames <erieadames@gmail.com>
2024-09-10 20:22:55 -0400

    test

--- assignment/test_usb.py
+++ assignment/test_usb.py
@@ -41,7 +41,7 @@ i2c = I2cController()
 
 # Use the correct URL based on your device details
 try:
-    i2c.configure('ftdi://ftdi:0403/6014/1')  # Ensure this URL is correct
+    i2c.configure('ftdi://ftdi:1027/24597/1')  # Ensure this URL is correct
     print("FTDI device configured successfully")
 except Exception as e:
     print(f"Error configuring I2C: {e}")
