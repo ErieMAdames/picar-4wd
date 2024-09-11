f21e0c9b95e398115b627cedd5487f4577439f1a
Erie M. Adames <erieadames@gmail.com>
2024-09-11 19:33:17 -0400

    test

--- assignment/part6_mapping.py
+++ assignment/part6_mapping.py
@@ -2,15 +2,11 @@ import numpy as np
 import picar_4wd as pc4
 import RPi.GPIO as GPIO
 import time
-import math
 from functools import reduce
 import sys
 import pygame
-# from mpu6050 import mpu6050
-import traceback
 import cv2
 np.set_printoptions(threshold=sys.maxsize)
-
 class Map():
     current_car_angle = 0
     speed = 30
@@ -38,12 +34,6 @@ class Map():
         pygame.init()
         self.screen = pygame.display.set_mode((500, 500))
         self.scan()
-        # x = input()
-        # traveled = self.go_distance(1, True)
-        # print(traveled)
-        # if traveled < 1:
-        #     self.avoid()
-            # sys.exit(0)
     def calibrate_turn_speed(self):
         start = time.time()
         try:
@@ -77,11 +67,6 @@ class Map():
             if distance > 0:
                 dx = int(distance * np.cos(np.radians(self.current_angle + 90))) + 50
                 dy = int(distance * np.sin(np.radians(self.current_angle + 90))) + 50
-                # print(self.current_angle + 90, distance,dx, dy)
-                
-                # Calculate the grid position
-                # object_x = car_x + dx
-                # object_y = car_y + dy
                 
                 # Check if the calculated position is within the grid
                 if dx >= 0 and dy >= 0 and dx < 100 and dy < 100:
@@ -115,105 +100,6 @@ class Map():
         # if stop:
         #     print(self.distances)
         self.distances = []
-
-    def avoid(self, right=True):
-        # if there is something in the way while avoiding, retrace stepts
-        print('avoiding')
-        retrace_steps = []
-        self.turn_right(90, self.speed)
-        turning_right = 'turning right' if right else 'turning left'
-        turning_left = 'turining left' if right else 'turningright'
-        print(turning_right)
-        stop = self.scan()
-        retrace_steps.append((not right, 0))
-        if stop:
-            print('obstacle')
-            # turn right to retrace
-            return retrace_steps
-        print('going')
-        dist = self.go_distance(self.forward_dist, True)
-        print(dist)
-        retrace_steps.append(('b', dist))
-        if dist < self.forward_dist:
-            print('obstacle')
-            return retrace_steps
-        self.turn_left()
-        print(turning_left)
-        stop = self.scan()
-        retrace_steps.append((right, 0))
-        if stop:
-            print('obstacle')
-            return retrace_steps
-        print('going')
-        dist = self.go_distance(self.forward_dist, True)
-        retrace_steps.append(('b', dist))
-        if dist < self.forward_dist:
-            print('obstacle')
-            return retrace_steps
-        self.turn_left()
-        print(turning_left)
-        stop = self.scan()
-        retrace_steps.append((right, 0))
-        if stop:
-            print('obstacle')
-            return retrace_steps
-        print('going')
-        dist = self.go_distance(self.forward_dist, True)
-        retrace_steps.append(('b', dist))
-        if dist < self.forward_dist:
-            print('obstacle')
-            return retrace_steps
-        self.turn_right()
-        print(turning_right)
-        stop = self.scan()
-        retrace_steps.append((not right, 0))
-        if stop:
-            print('obstacle')
-            return retrace_steps
-        print('going')
-        dist = self.go_distance(self.forward_dist, True)
-        retrace_steps.append(('b', dist))
-        if dist < self.forward_dist:
-            print('obstacle')
-            return retrace_steps
-        return []
-    def retrace(self, retrace_steps):
-        print('retracing')
-        for step in reversed(retrace_steps):
-            if step[0] == 'b':
-                self.go_distance(step[1], False)
-            else:
-                self.turn(step[0], 90, self.speed)
-                
-
-    def go_distance(self, dist, forward=True):
-        self.left_encoder_count = 0
-        self.right_encoder_count = 0
-        def calculate_distance(counts):
-            wheel_circumference = self.WHEEL_DIAMETER * 3.14159
-            distance = (counts / self.PPR) * wheel_circumference
-            return distance
-
-        left_distance = calculate_distance(self.left_encoder_count)
-        right_distance = calculate_distance(self.right_encoder_count)
-        
-        while left_distance < dist or right_distance < dist:
-            stop = self.scan()
-            if stop:
-                break
-            if forward:
-                pc4.forward(self.speed)
-            else:
-                pc4.backward(self.speed)
-            left_distance = calculate_distance(self.left_encoder_count)
-            right_distance = calculate_distance(self.right_encoder_count)
-            # print(left_distance)
-            # print(right_distance)
-            # print('----')
-        pc4.stop()
-        time.sleep(.5)
-        return min(left_distance,right_distance)
-
     def turn_right(self,  angle=90, speed=30):
         pc4.turn_right(speed)
         time.sleep(self.turning_time)
