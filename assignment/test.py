689ba62d85936b1a25336468f5db31520b259b2f
Erie M. Adames <erieadames@gmail.com>
2024-09-14 15:46:27 -0400

    hope

--- assignment/test.py
+++ assignment/test.py
@@ -107,11 +107,11 @@ while running:
     # Run object detection
     detection_result = detector.detect(input_tensor)
     rgb_image = visualize(rgb_image, detection_result)
-    rgb_image = cv2.flip(rgb_image, 0)
     # Display FPS
     fps_text = f'FPS = {fps:.1f}'
     cv2.putText(rgb_image, fps_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, _TEXT_COLOR, 2)
 
+    rgb_image = cv2.flip(rgb_image, 0)
     # Convert RGB to Pygame format
     rgb_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2RGB) 
     # Ensure image is of shape (height, width, 3) for Pygame
