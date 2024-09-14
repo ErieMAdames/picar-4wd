0f623dd44a0cc54b7bc5b2d80630b4a2317077b5
Erie M. Adames <erieadames@gmail.com>
2024-09-14 18:29:01 -0400

    hope

--- assignment/stream_video.py
+++ assignment/stream_video.py
@@ -65,7 +65,9 @@ class Map:
             fps_text = 'FPS = {:.1f}'.format(fps)
             cv2.putText(image, fps_text, (24, 20), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
             image = cv2.flip(image, 1)
-
+            for detection in detection_result.detections:
+                if detection.categories[0].index == 12:
+                    pc4.stop()
             # Update global frames for Flask and Pygame
             frame = image
             pygame_frame = image
