0a47529ef428ede579e48414707af836e95b409e
Erie M. Adames <erieadames@gmail.com>
2024-09-14 11:58:34 -0400

    hope

--- assignment/test.py
+++ assignment/test.py
@@ -64,6 +64,7 @@ while running:
     # Capture frame from the camera
     image = picam2.capture_array("main")
     image = cv2.flip(image, 1)
+    image = cv2.flip(image, 0)
 
     # Calculate FPS
     counter += 1
@@ -86,7 +87,6 @@ while running:
 
     # Convert image from BGR to RGB format required by Pygame (already in RGB format for TFLite)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    # image = cv2.flip(image, 0)
     
     # Ensure image is of shape (height, width, 3) for Pygame
     if image.shape[2] == 3:
