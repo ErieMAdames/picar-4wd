455fdcf0f921db2507d745a6e6c4814123193bf8
Erie M. Adames <erieadames@gmail.com>
2024-09-15 00:13:20 -0400

    hope

--- assignment/calibrate_speed.py
+++ assignment/calibrate_speed.py
@@ -1,9 +1,10 @@
 import picar_4wd as pc4
 import time
-
-start = time.time()
-pc4.forward(1)
-time.sleep(4.2 * (float(input()) / 100))
-stop = time.time()
-pc4.stop()
-print(stop - start)
+while True:
+    dist = input()
+    start = time.time()
+    pc4.forward(1)
+    time.sleep(4.2 * (float(dist) / 100))
+    stop = time.time()
+    pc4.stop()
+    print(stop - start)
