727998066c419e6f457fe37741831724ee1dd468
Erie M. Adames <erieadames@gmail.com>
2024-09-15 11:01:33 -0400

    hope

--- assignment/calibrate.py
+++ assignment/calibrate.py
@@ -1,18 +1,7 @@
-from mpu6050 import mpu6050
-import time
-imu = mpu6050(0x68)
-x = 0
-z = 0
-y = 0
-counter = 0
+import picar_4wd as pc4
 try:
     while True:
-        g = imu.get_gyro_data()
-        x += g['x']
-        y += g['y']
-        z += g['z']
-        counter += 1
+        angle = float(input())
+        pc4.get_distance_at(angle)
 except KeyboardInterrupt:
-    print('X ' + str(x / counter))
-    print('Y ' + str(y / counter))
-    print('Z ' + str(z / counter))
+    exit()
