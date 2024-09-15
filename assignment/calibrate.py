87610f30c6489feced4461a6515cc734795ee15c
Erie M. Adames <erieadames@gmail.com>
2024-09-15 11:02:19 -0400

    hope

--- assignment/calibrate.py
+++ assignment/calibrate.py
@@ -2,6 +2,6 @@ import picar_4wd as pc4
 try:
     while True:
         angle = float(input())
-        pc4.get_distance_at(angle)
+        print(pc4.get_distance_at(angle))
 except KeyboardInterrupt:
     exit()
