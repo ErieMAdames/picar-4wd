92c5713ce572ec0a1278ca57e8e0ec26dc21ff5e
Erie M. Adames <erieadames@gmail.com>
2024-10-06 22:32:54 -0400

    resolution

--- assignment/stream_video.py
+++ assignment/stream_video.py
@@ -27,7 +27,7 @@ width, height = 3280, 2464
 low_res_width, low_res_height = 640, 480
 global_fps_max = 0
 class Map:
-    base_options = core.BaseOptions(file_name='efficientdet_lite0.tflite', use_coral=False, num_threads=4)
+    base_options = core.BaseOptions(file_name='efficientdet_lite0.tflite', use_coral=True, num_threads=4)
     detection_options = processor.DetectionOptions(max_results=1, score_threshold=0.5)  # Limit to 1 result for speed
     options = vision.ObjectDetectorOptions(base_options=base_options, detection_options=detection_options)
     detector = vision.ObjectDetector.create_from_options(options)
