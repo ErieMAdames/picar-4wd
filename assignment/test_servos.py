f33f23eed958a6108da0e45cdebe275979bc8256
Erie M. Adames <erieadames@gmail.com>
2024-10-26 18:20:53 -0400

    testing

--- assignment/test_servos.py
+++ assignment/test_servos.py
@@ -5,64 +5,26 @@ from picar_4wd.servo import Servo
 from picar_4wd.pwm import PWM
 import os
 
-# Setup your servos and initial angles
-servo0_angle_offset = 0
-servo1_angle_offset = -8
-servo2_angle_offset = 0
-servo0_angle = 0
-servo1_angle = 0
-servo2_angle = 0
-servo0 = Servo(PWM("P0"))
-servo1 = Servo(PWM("P1"))
-servo2 = Servo(PWM("P2"))
+# Setup your servos and initial angles (same as before)
 
-servo0.set_angle(servo0_angle_offset)
-servo1.set_angle(servo1_angle_offset)
-servo2.set_angle(servo2_angle_offset)
-
-# Set ulimit for file descriptors
-os.system('ulimit -n 4096')
-
-# Start RTSP stream using ffmpeg
+# Start RTSP stream using ffmpeg and log output
 ffmpeg_command = [
     'ffmpeg', '-f', 'v4l2', '-i', '/dev/video0', '-preset', 'ultrafast',
     '-f', 'rtsp', 'rtsp://192.168.86.46:8554/stream'
 ]
-ffmpeg_process = subprocess.Popen(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+ffmpeg_process = subprocess.Popen(
+    ffmpeg_command,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    text=True
+)
 
-HOST = "192.168.86.46" # IP address of your Raspberry PI
-PORT = 65432
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    try:
-        while 1:
-            client, clientInfo = s.accept()
-            data = client.recv(1024)
-            if data != b"":
-                print(data)
-                if (data == b"up"):
-                    servo2_angle = max(servo2_angle - 2 + servo2_angle_offset, -90)
-                    servo2.set_angle(servo2_angle)
-                elif (data == b"down"):
-                    servo2_angle = min(servo2_angle + 2 + servo2_angle_offset, 90)
-                    servo2.set_angle(servo2_angle)
-                elif (data == b"left"):
-                    servo1_angle = max(servo1_angle - 2 + servo1_angle_offset, -90)
-                    servo1.set_angle(servo1_angle)
-                elif (data == b"right"):
-                    servo1_angle = min(servo1_angle + 2 + servo1_angle_offset, +90)
-                    servo1.set_angle(servo1_angle)
-                client.sendall(str.encode(json.dumps({
-                    'data': data.decode("utf-8"),
-                    'servo1_angle': servo1_angle,
-                    'servo2_angle': servo2_angle
-                }))) # Echo back to client
-    except (KeyboardInterrupt, Exception) as e:
-        print(e)
-        print("Closing socket and terminating stream")
-        ffmpeg_process.terminate()  # Stop ffmpeg when done
-        ffmpeg_process.wait()  # Wait for ffmpeg to fully close
-        client.close()
-        s.close()
-socket.socket.close()
+# Log ffmpeg output continuously in a separate thread or process
+import threading
+def log_ffmpeg_output(process):
+    for line in process.stderr:
+        print("FFmpeg output:", line)
+
+threading.Thread(target=log_ffmpeg_output, args=(ffmpeg_process,)).start()
+
+# Socket server setup (same as before)
