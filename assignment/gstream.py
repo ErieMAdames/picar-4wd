f2801790cd2104c3fa68e39427b2ffb1fac907f5
Erie M. Adames <erieadames@gmail.com>
2024-10-27 01:09:24 -0400

    testing

--- assignment/gstream.py
+++ assignment/gstream.py
@@ -4,7 +4,8 @@ from gi.repository import Gst, GstRtspServer, GObject, GLib
 class RTSPServer(GstRtspServer.RTSPMediaFactory):
     def __init__(self):
         super(RTSPServer, self).__init__()
-        self.set_launch("( v4l2src device=/dev/video0 ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay name=pay0 pt=96 )")
+        self.set_launch("( v4l2src device=/dev/video0 ! video/x-raw,format=I420,width=320,height=240,framerate=15/1 ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay config-interval=1 pt=96 )")
+
 
 # GObject.threads_init()
 Gst.init(None)
